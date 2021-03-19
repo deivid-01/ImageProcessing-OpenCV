e2fb78d62ffc23b6a88a4d88113d273e6e001c4c
deivid-01 <datcx@hotmail.com>
2021-03-19 14:17:35 -0500

    hot fix

--- Assets/Scripts/OpenCv_MovDetection.cs
+++ Assets/Scripts/OpenCv_MovDetection.cs
@@ -7,6 +7,7 @@ public class OpenCv_MovDetection : MonoBehaviour
     public AnimationCurve plot = new AnimationCurve();
 
     WebCamTexture webCamTexture;
+    public Texture2D texture;
 
     public List<double> snapsValues = new List<double>(); 
     List<Mat> mats = new List<Mat>();
@@ -18,11 +19,47 @@ public class OpenCv_MovDetection : MonoBehaviour
         webCamTexture = new WebCamTexture(devices[0].name);
 
         webCamTexture.Play();
-       // GetComponent<Renderer>().material.mainTexture = webCamTexture;
-        StartCoroutine(BasicDetectorMovement());
+       
+
+        CreateMask();
+        //StartCoroutine(BasicDetectorMovement());
     }
 
-   
+
+    private void Update()
+    {
+     /*   
+        Mat result = OpenCvSharp.Unity.TextureToMat(webCamTexture);
+     
+
+        Mat mat = new Mat(result.Height, result.Width, MatType.CV_8UC3, new Scalar(0, 0, 0));
+    
+
+         mat[new OpenCvSharp.Rect(100, 100, 50, 50)].SetTo(Scalar.All(255));
+
+ 
+
+
+        for (int i = 0; i < mat.Height; i++)
+        {
+            for (int j = 0; j < mat.Width; j++)
+            {
+                if (mat.At<Vec3b>(new int[2] { i, j }).Item0 == 255)
+                    
+                    continue;
+                Vec3b vec = new Vec3b(100, 100, 100);
+                result.Set<Vec3b>(new int[2] { i, j }, vec);
+
+            }
+        }
+
+       
+
+        GetComponent<Renderer>().material.mainTexture = OpenCvSharp.Unity.MatToTexture(result);
+        
+        */
+    }
+
     // Update is called once per frame
 
     IEnumerator BasicDetectorMovement()
@@ -42,11 +79,11 @@ public class OpenCv_MovDetection : MonoBehaviour
                 if ((sum.Val0 + sum.Val1 + sum.Val2) > 10000000)
                 {
                     print("TARGET IS MOVING");
-                    GetComponent<Renderer>().material.color = Color.red;// OpenCvSharp.Unity.MatToTexture(result);
+                    GetComponent<Renderer>().material.mainTexture = OpenCvSharp.Unity.MatToTexture(result);
                 }
-                else {
-                    GetComponent<Renderer>().material.color = Color.black;
-                }
+                
+            
+               
                     
                     //snapsValues.Add((sum.Val0 + sum.Val1 + sum.Val2));
                    // plot.AddKey(snapsValues.Count,(float) snapsValues[snapsValues.Count-1]);
@@ -61,6 +98,47 @@ public class OpenCv_MovDetection : MonoBehaviour
         StartCoroutine(BasicDetectorMovement());
 
 
+    }
 
+    void CreateMask()
+    {
+
+        Mat original = OpenCvSharp.Unity.TextureToMat(texture);
+        Mat result = OpenCvSharp.Unity.TextureToMat(texture); //new Mat(720, 1200, MatType.CV_8UC3, new Scalar(0, 0, 0));
+      
+        Mat mat = new Mat(result.Height, result.Width, MatType.CV_8UC3, new Scalar(0, 0, 0));
+      
+        mat[new OpenCvSharp.Rect(result.Width/2, result.Height/2, 100, 100)].SetTo(Scalar.All(255));
+
+        for (int i = 0; i < mat.Height; i++)
+        {
+            for (int j = 0; j < mat.Width; j++)
+            {
+                if (mat.At<Vec3b>(new int[2] { i, j })[0] != 255)
+                {
+
+                    //mat.Set<int>(new int[2] { i, j }, 255);
+                    Vec3b vec = new Vec3b(0, 0, 0);
+                    result.Set<Vec3b>(new int[2] { i, j }, vec);
+
+
+                }
+                else
+                {
+                    result[new OpenCvSharp.Rect(i,j, 1, 1)].SetTo(Scalar.All(0));
+
+                    //result.Set<Vec3b>(new int[2] { i, j }, original.At<Vec3b>(new int[2] { i, j }));
+                }
+              
+
+            }
+        }
+
+
+        
+
+
+         GetComponent<Renderer>().material.mainTexture = OpenCvSharp.Unity.MatToTexture(mat);
+        Debug.Log("oliwis");
     }
 }
